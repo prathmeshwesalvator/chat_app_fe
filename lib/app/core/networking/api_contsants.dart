44a2b34533efd22a7d1f63fe8b279767a0554434
Prathmesh Wesalvator <prathmesh.wesalvator@gmail.com>
2026-02-01 12:08:32 +0530

    added server urls

--- lib/app/core/networking/api_contsants.dart
+++ lib/app/core/networking/api_contsants.dart
@@ -1,7 +1,9 @@
 class ApiConstants {
-  static const String baseUrl = 'http://0.0.0.0:8000';
-  // static const String baseUrl = 'https://chat-app-be-b7cs.onrender.com';
-  static const String webSocketUrl = 'ws://127.0.0.1:8000/ws/chat/general/';
+  // static const String baseUrl = 'http://0.0.0.0:8000';
+  // static const String webSocketUrl = 'ws://127.0.0.1:8000/ws/chat/general/';
+  static const String baseUrl = 'https://chat-app-be-b7cs.onrender.com';
+  static const String webSocketUrl =
+      'ws://chat-app-be-b7cs.onrender.com/ws/chat/general/';
   static const String contentType = 'content-type';
   static const String contentJson = 'application/json';
   static const String contentMultipart = 'multipart/form-data';
